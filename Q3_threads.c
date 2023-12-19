b61fe8774b9996b5315d7ec3cfbe2730a6b498d2
MXNXV-ERR <manavrai.jmn@gmail.com>
2023-12-19 21:43:18 +0530

    it was always 3

--- Q3_threads.c
+++ Q3_threads.c
@@ -7,7 +7,7 @@ void main(){
     scanf("%d",&n);
     
 
-    #pragma omp parallel for schedule(static,2)
+    #pragma omp parallel for schedule(static,3)
     for(i=1; i<=n; i++)
         printf("thread %d itern %d value: %d\n",omp_get_thread_num(),itern[omp_get_thread_num()]++,i);
         
